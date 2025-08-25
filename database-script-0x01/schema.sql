5eb43f3ef070655eac3f69c37049fcc89678d191
Moussa Mahamat Nassour <132145833+Moussassoss@users.noreply.github.com>
2025-08-25 14:32:40 +0200

    Create schema.sql

---
+++ database-script-0x01/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Users (
+    user_id UUID PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20),
+    role ENUM('guest', 'host', 'admin') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
